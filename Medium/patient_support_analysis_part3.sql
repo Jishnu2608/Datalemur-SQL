6cff51c2c779cdcf7c7580f504f0710dac2cebae
Jishnudeep Borah <85721902+Jishnu2608@users.noreply.github.com>
2026-04-20 18:13:42 +0530

    done

--- Medium/patient_support_analysis_part3.sql
+++ Medium/patient_support_analysis_part3.sql
@@ -3,4 +3,4 @@ lag(call_date) over (partition by policy_holder_id order by call_date) as prev_c
 from callers)
 
 select count(distinct policy_holder_id) as policy_holder_count 
-from cte where prev_call > call_date - interval '7 days'
+from cte where prev_call > date_sub(call_date, interval 7 day)
